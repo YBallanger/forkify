5211aebc6e7443fc62c6e56ff1941d569323b8fa
YBallanger <yann.ballanger@gmail.com>
2024-12-21 00:35:26 +0100

    :sparkles: feat : appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forkify/res/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +10,31 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      home: Builder(
+        builder: (context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconButton(onPressed: () => {}, icon: Icon(Icons.menu)),
+                  Text("Forkify"),
+                  IconButton(
+                    onPressed: () => {},
+                    icon: Icon(Icons.account_circle_outlined))
+                ],
+              ),
+            ),
+            body: Center(
+              child: const Text("HELLO WORLD !"),
+            ),
+            bottomNavigationBar: Container(
+                height: 100, color: Theme.of(context).colorScheme.primary),
+          );
+        },
       ),
     );
   }
---
+++ lib/res/theme/color_schemes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xffe06742),
+  onPrimary: Color(0xffffffff),
+  secondary: Color(0xffc4a9a1),
+  onSecondary: Color(0xff141212),
+  error: Color.fromARGB(255, 204, 25, 25),
+  onError: Color.fromARGB(255, 28, 27, 27),
+  surface: Color(0xfff5f1ef),
+  onSurface: Color(0xff000000),
+  primaryContainer: Color(0xffebe2e0),
+  onPrimaryContainer: Color(0xff1f1614),
+);
