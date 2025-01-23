208c5e3bc0279169201b2a28b788640a3c511acb
YBallanger <yann.ballanger@gmail.com>
2025-01-23 22:56:18 +0100

    :sparkles: bouton ajout experience

--- lib/view/screen/home.screen.dart
+++ lib/view/screen/home.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forkify/res/fonts.dart';
 import 'package:forkify/view/feature/someStatsCard/some_stats_card.feature.dart';
 import 'package:forkify/view/feature/topAdresses/top_addresses.feature.dart';
 
@@ -9,17 +10,46 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            TopAddresses(),
-            SizedBox(height: 20,),
-            SomeStatsCard()
-          ],
+    return Stack(
+      children: [
+        SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                TopAddresses(),
+                SizedBox(
+                  height: 20,
+                ),
+                SomeStatsCard(),
+                SizedBox(
+                  height: 70,
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SizedBox(
+              height: 50,
+              child: FilledButton.icon(
+                onPressed: () => {},
+                icon: Icon(
+                  Icons.add_circle_outline,
+                  size: 25,
+                ),
+                label: Text(
+                  "Ajouter une expérience",
+                  style: Fonts.bodyLarge,
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
